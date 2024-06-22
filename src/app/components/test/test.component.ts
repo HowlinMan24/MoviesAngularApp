4610e4361559333fa63d9bd0ba1d9ec7d9d1cd22
Hristijan.Mijalkov.LoL <115577273+HowlinMan@users.noreply.github.com>
2024-06-23 00:39:14 +0200

    Initial commit

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h1>Welcome to test componet</h1>
+    <p>This is a test componet</p>
+  `,
+  styles: ['h1 {color: red}']
+})
+
+export class TestComponent {
+
+}
